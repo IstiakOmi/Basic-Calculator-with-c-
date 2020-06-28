e4dca1c275e7a756de60c5b3ed1feb81f09e73c9
IstiakOmi <anik.cse3.bu@gmail.com>
2020-06-27 23:14:52 -0700

    first commit

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Dynamic;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            double numb1 =Convert.ToDouble(Console.ReadLine());
+            //Because Console.ReadLine() took data only in string format.
+
+            Console.WriteLine("Enter another number:");
+            double numb2 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Write("The Addition is: " );
+            Console.WriteLine(numb1 + numb2);
+
+            Console.Write("The Substraction is: ");
+            Console.WriteLine(numb1 - numb2);
+
+            Console.Write("The multiplication is: ");
+            Console.WriteLine(numb1 * numb2);
+
+            Console.Write("The division is: ");
+            Console.WriteLine(numb1 / numb2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
